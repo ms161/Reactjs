c30f1ae3520fc4dee22de85a71945658b69907d3
ms161 <115150076+ms161@users.noreply.github.com>
2023-05-29 01:59:15 +0530

    using useState multiple times

--- ExpenseForm.jsx
+++ ExpenseForm.jsx
@@ -3,22 +3,39 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  function con(e) {
-    console.log(e.target.value);
+  // function con(e) {
+  //   console.log(e.target.value);
+  // }
+
+  const [enterTitle,setEnterdTitle]=useState('')
+  const [enterAmount,setEnterdAmount]=useState('')
+  const [enterDate,setEnterdDate]=useState('')
+  function titleChanger(e){
+    setEnterdTitle(e.target.value)
+    console.log(enterTitle)
   }
+  function amountChanger(e){
+    setEnterdAmount(e.target.value)
+    console.log(enterTitle)
+  }
+  function dateChanger(e){
+    setEnterdDate(e.target.value)
+    console.log(enterTitle)
+  }
+
 
   return (
     <form>
-      <div onChange={con} className="new-expense__controls">
+      <div onChange={titleChanger} className="new-expense__controls">
         <div className="new-expense">
           <label>Title</label>
           <input type="text" />
         </div>
-        <div onChange={con} className="new-expense">
-          <label>Amount</label>
+        <div onChange={amountChanger} className="new-expense">
+          <label>amount</label>
           <input type="number" min="0.01" step="0.01" />
         </div>
-        <div onChange={con} className="new-expense">
+        <div onChange={dateChanger} className="new-expense">
           <label>Date</label>
           <input type="date" min="2019-01-01" max="2022-12-31" />
         </div>
