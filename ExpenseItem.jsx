246f89728afb5e90b5abfb7dc4084b7cffa9f759
ms161 <115150076+ms161@users.noreply.github.com>
2023-05-31 02:18:29 +0530

    Expenses Filter

--- ExpenseItem.jsx
+++ ExpenseItem.jsx
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+
 
 // useState() //we cant call this here
 const ExpenseItem = (props) => {
@@ -13,26 +15,27 @@ const ExpenseItem = (props) => {
   //the title is the value , and setTitle is updated function from which we can change the value of title
 
   const click = (e) => {
-setTitle('Updated!')
-setAmount(100)
-//when we call setTitle() the jsx file will get executed again and thats how we get our new data rendered to screen again 
+    setTitle("Updated!");
+    setAmount(100);
+    //when we call setTitle() the jsx file will get executed again and thats how we get our new data rendered to screen again
     // e.target.parentElement.remove();
-    console.log(title)
-
+    console.log(title);
   };
+const [filterdYear, setFilterdYear]=useState('2023')
+
+  const filterChangeHandler=selectedyear=>{
+    setFilterdYear(selectedyear)
+  }
 
   return (
     <Card className="expense-item2">
+      <ExpensesFilter selected={filterdYear} onChangeFilter={filterChangeHandler} />
+
       <ExpenseDate date={props.date} />
 
-      <ExpenseDetails
-        name={title}
-        amount={amount}
-        location={props.location}
-      />
+      <ExpenseDetails name={title} amount={amount} location={props.location} />
 
       <button onClick={click}>Delete Expense</button>
-      
     </Card>
   );
 };
